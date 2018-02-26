1da987952bd940c0712365ff7661603f56a0ceae
Amol Pachpute <amol.pachpute@CALSOFTHQ.COM>
2018-02-26 14:08:07 +0530

    deleted

--- analyze/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, include, url
-from analyze.views import Analyze
-urlpatterns = patterns('',
-    # Examples:
-    url(r'^$', Analyze.as_view(), name='analyze'),
-
-)
--- compare/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, include, url
-from compare.views import Compare
-urlpatterns = patterns('',
-    # Examples:
-    url(r'^$', Compare.as_view(), name='compare'),
-
-)
